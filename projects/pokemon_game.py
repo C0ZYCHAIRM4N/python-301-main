b50c4ae5f0a755cab61adcee6a214b9d2edf78e2
Your Name <d1g1man129@gmail.com>
2024-09-18 15:05:27 -0500

    done

--- projects/pokemon_game.py
+++ projects/pokemon_game.py
@@ -11,3 +11,41 @@
 # - Display messages that explain who won or lost a battle
 # - If a Pokemon loses a battle, they lose some of their `hp`
 # - If you call the `feed()` method on a Pokemon, they regain some `hp`
+class Pokemon:
+    def __init__(self, name, primary_type, max_hp):
+        self.name = name
+        self.primary_type = primary_type
+        self.max_hp = max_hp
+        self.hp = max_hp
+
+    def battle(self, opponent):
+        if self.primary_type == "water" and opponent.primary_type == "fire":
+            print(f"{self.name} wins!")
+        elif self.primary_type == "fire" and opponent.primary_type == "grass":
+            print(f"{self.name} wins!")
+        elif self.primary_type == "grass" and opponent.primary_type == "water":
+            print(f"{self.name} wins!")
+        else:
+            print(f"{opponent.name} wins!")
+            self.hp -= 10
+
+    def feed(self):
+        self.hp += 10
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp
+        print(f"{self.name} has been fed and now has {self.hp} hp.")
+
+# Instantiate Pokemon objects
+charmander = Pokemon("Charmander", "fire", 50)
+squirtle = Pokemon("Squirtle", "water", 50)
+bulbasaur = Pokemon("Bulbasaur", "grass", 50)
+
+charmander.battle(squirtle)
+
+
+
+
+
+
+
+
