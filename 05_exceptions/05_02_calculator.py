8991143e2261e4c15cb45c4c9c919689175f3bb6
Your Name <d1g1man129@gmail.com>
2024-09-27 11:07:31 -0500

    done

--- 05_exceptions/05_02_calculator.py
+++ 05_exceptions/05_02_calculator.py
@@ -5,3 +5,30 @@
 # - if the user enters a zero as the divisor
 #
 # Test it and make sure it does not crash when you enter incorrect values.
+numbers = []
+
+try:
+    for i in range(2):
+        num = int(input("enter 2 numbers: "))
+        numbers.append(num)
+    quotient = numbers[0] / numbers[1]
+    print(quotient)
+except ValueError:
+    print("you didn't enter a number!")
+except ZeroDivisionError:
+    print("Error: Division by zero is not allowed.")
+
+
+
+    
+
+
+    
+
+
+
+
+
+
+
+
