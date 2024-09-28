f598fb13c8c63e26ea0c11adb2fe50f90ae14b06
Your Name <d1g1man129@gmail.com>
2024-09-28 15:27:26 -0500

    done

--- 05_exceptions/05_03_check_for_ints.py
+++ 05_exceptions/05_03_check_for_ints.py
@@ -2,3 +2,10 @@
 # validity of the input type, and displays a message depending on whether
 # the input was an integer or not.
 # The script should keep prompting the user until they enter an integer.
+while True:
+    try:
+        num = int(input("Enter number: "))
+        print(f"You entered the number: {num}")
+        break  # Exit the loop if the input is a valid number
+    except ValueError:
+        print("That's not a valid number. Please try again.")
