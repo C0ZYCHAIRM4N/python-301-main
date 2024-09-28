513c8392157244eb6ec48fdfb785e508eaeddfe9
Your Name <d1g1man129@gmail.com>
2024-09-28 18:01:31 -0500

    done

--- 05_exceptions/05_04_else_database.py
+++ 05_exceptions/05_04_else_database.py
@@ -1,4 +1,20 @@
 # Write a script that demonstrates a try/except/else statement.
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
+
 # the database connection can be established.
+import sqlalchemy
+
+try:
+    # attempt to connect to database  
+    engine = sqlalchemy.create_engine('mysql+pymysql://username:password@localhost/mydatabase')
+    print("you are now connected to database")
+    connection = engine.connect()
+except:
+    print("connection unsuccesful")
+else:
+    print("Connection established, and can now perform database operations.")
+
+
+
+
