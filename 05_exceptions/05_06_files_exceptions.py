7004a81d53d03451925218cfff1dbeedc09d1f10
Your Name <d1g1man129@gmail.com>
2024-09-28 18:59:11 -0500

    done

--- 05_exceptions/05_06_files_exceptions.py
+++ 05_exceptions/05_06_files_exceptions.py
@@ -11,5 +11,21 @@
 # 2) Open `crime_and_punishment.txt` and overwrite the whole content with an empty string
 # 3) Loop over all three files and print out only the first character each. Your program
 #    should NEVER terminate with a Traceback.
-#     a) Which exception can you expect to encounter? Why?
-#     b) How do you catch it to avoid the program from terminating with a traceback?
+#     a) Which exception can you expect to encounter? Why? -------unicodedecodeerror
+#     b) How do you catch it to avoid the program from terminating with a traceback? 
+
+try:
+    file_in = open(r"C:\Users\speng\Downloads\python-301-main\python-301-main\05_exceptions\books\war_and_peace.txt", 'r')
+    contents = file_in.read()
+except UnicodeDecodeError as e:
+    print(f"Error reading file: {e}")
+finally:
+    try:
+        file_in.close()
+    except NameError:
+        pass
+
+# Clear the contents of the file
+with open(r"C:\Users\speng\Downloads\python-301-main\python-301-main\05_exceptions\books\war_and_peace.txt", 'w') as war_and_peace:
+    war_and_peace.write("")
+
