41b29244d44ad065f84fcecd64b7b50ce4aeb4a8
Your Name <d1g1man129@gmail.com>
2024-09-28 19:40:38 -0500

    done

--- 05_exceptions/05_07_find_a_prince.py
+++ 05_exceptions/05_07_find_a_prince.py
@@ -3,3 +3,25 @@
 # like you did in the previous exercise.
 # Raise your `PrinceException()` if the first 100 characters of a book
 # contain the string "Prince".
+
+
+class PrinceException(Exception):
+    pass
+
+
+
+try:
+    file_in = open(r"C:\Users\speng\Downloads\python-301-main\python-301-main\05_exceptions\books\war_and_peace.txt", 'r', encoding='utf-8')
+    contents = file_in.read(100)
+    if "Prince" in contents:
+        raise PrinceException("The string 'Prince' was found in the first 100 characters of the book.")
+except UnicodeDecodeError as e:
+    print("UnicodeDecodeError: whoops")
+except PrinceException as e:
+    print(e)
+finally:
+    file_in.close()
+
+ 
+
+
