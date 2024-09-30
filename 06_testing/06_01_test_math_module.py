982f94e70aface6dc01647f6ea9a9ef7ce575fc9
Your Name <d1g1man129@gmail.com>
2024-09-29 19:22:44 -0500

    done

--- 06_testing/06_01_test_math_module.py
+++ 06_testing/06_01_test_math_module.py
@@ -1,2 +1,17 @@
 # Write a unittest test suite with at least two methods that test
 # the functionality of the built-in `math` module.
+import unittest
+import math
+
+class TestMath(unittest.TestCase):
+
+    def test_floor_rounds_down(self):
+        self.assertEqual(math.floor(3, 4), 3)
+
+    def test_sqrt(self):
+        self.assertEqual(math.sqrt(4), 2)
+        self.assertEqual(math.sqrt(9), 3)
+        self.assertAlmostEqual(math.sqrt(2), 1.41421356237, places=10)
+
+if __name__ == '__main__':
+    unittest.main()
