a0eb27cfed2727a20916aea8b7750af90077f732
Kyle <c0zychair@outlook.com>
2024-11-09 08:37:25 -0600

    done

--- 02_classes-objects-methods/02_03_car.py
+++ 02_classes-objects-methods/02_03_car.py
@@ -6,3 +6,32 @@
 #
 # Create at least two different objects of this `Car()` class and demonstrate
 # changing the objects' attributes.
+
+class Car:
+    """Creates an empty car object"""
+    def __init__(self, model, year, max_speed):
+        self.model = model
+        self.year = year
+        self.max_speed = max_speed
+    
+    def increase_speed(self):
+        """increase max speed by 5"""
+        self.max_speed += 5
+        print(f"{self.max_speed}")
+        
+    
+    def details_of_car(self):
+        """prints details of car"""
+        print(f"{self.model} {self.year} {self.max_speed}")
+
+
+car = Car("honda", 1990, 80)
+car.increase_speed()
+
+car = Car("chevy", 2016, 100)
+
+
+
+
+
+        
