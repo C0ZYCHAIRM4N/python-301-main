3fadc4938ba9a88577a404f8a6d64845afb490b2
Kyle <c0zychair@outlook.com>
2024-11-09 08:40:23 -0600

    done

--- 02_classes-objects-methods/02_02_planets.py
+++ 02_classes-objects-methods/02_02_planets.py
@@ -3,14 +3,21 @@
 # Use the appropriate dunder method to get informative output with `print()`
 
 class Planet():
-    """_summary_
+    """_blueprint for a planet
     """
     def __init__(self, name, diameter, distance_from_sun, moons):
         self.name = name
         self.diameter = diameter
         self.distance_from_sun = distance_from_sun
         self.moons = moons
-    pass
-print(Planet("Earth", 12742, 93, 1))
+
+    def __str__ (self):
+        return f"{self.name} {self.diameter} {self.distance_from_sun} {self.moons}"
+    
+planet = Planet("mars", 1000, 1234, 3)
+
+print(planet)
+    
+    
 
 
