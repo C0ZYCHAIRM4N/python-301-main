36e54cdd520da6ada8b158d24ee387aa5ed94a05
Kyle <c0zychair@outlook.com>
2024-11-23 09:06:56 -0600

    done

--- 04_web-scraping/04_07_refactor_rescrape.py
+++ 04_web-scraping/04_07_refactor_rescrape.py
@@ -2,3 +2,17 @@
 # separate functions. This is a great exercise to revisit writing functions
 # as well as for refactoring your code. It'll also help you in an upcoming
 # section of the course when you'll write tests for your web scraper.
+
+import requests
+
+
+def get_filtered_links(url):
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    links = soup.find_all('a', href=True)
+    filtered_links = []
+    for link in links:
+        href = link['href']
+        if not href.startswith('#') and 'mw-jump-link' not in link.get('class', []):
+            filtered_links.append(href)
+    return filtered_links
