4ebb64fbe6a6a6003f96135cd99951b46dbc32ac
Kyle <c0zychair@outlook.com>
2024-11-23 09:04:17 -0600

    done

--- 04_web-scraping/04_06_web_scraping.py
+++ 04_web-scraping/04_06_web_scraping.py
@@ -4,3 +4,10 @@
 # BE RESPECTFUL! Don't scrape sites that don't want to be scraped, and
 # limit the amount of calls you make to their page by saving the response
 # to a file, and parsing the content from that file.
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.azlyrics.com/lyrics/queen/bohemianrhapsody.html"
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+print(soup.prettify())
