42a54524f89d497362b3cc11989b4c9a694d23b8
Kyle <c0zychair@outlook.com>
2024-12-24 08:49:36 -0600

    done

--- 06_testing/06_05_recipe-scraper/test_rescrape.py
+++ 06_testing/06_05_recipe-scraper/test_rescrape.py
@@ -2,3 +2,5 @@
 
 import unittest
 import rescrape
+
+# make request to the API
--- 07_decorators/07_01_quote_wrap.py
+++ 07_decorators/07_01_quote_wrap.py
@@ -1,3 +1,15 @@
 # Write a decorator function that wraps text output into quotes, e.g.:
 # Hello world! ----> "Hello World!"
 # You can use it to create quotes from text output.
+
+def quote_wrap(func):
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return f'"{result}"'
+    return wrapper
+
+@quote_wrap
+def say_hello():
+    return "Hello world!"
+
+print(say_hello())  
