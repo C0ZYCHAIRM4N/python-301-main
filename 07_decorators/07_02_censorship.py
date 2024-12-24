2f5e471dfe18521606deb1c8c39dcc0752fa72da
Kyle <c0zychair@outlook.com>
2024-12-24 08:53:48 -0600

    done

--- 07_decorators/07_02_censorship.py
+++ 07_decorators/07_02_censorship.py
@@ -4,3 +4,20 @@
 #    "I bumped my toe! Shoot!"
 # Would, after decorating it with `@censor()`, return:
 #    "I bumped my toe! S****!"
+
+def censor(offensive_words):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            for word in offensive_words:
+                result = result.replace(word, word[0] + '*' * (len(word) - 1))
+            return result
+        return wrapper
+    return decorator
+
+@censor(["shoot", "darn"])
+def get_user_message():
+    return input("Enter your message: ")
+
+print(get_user_message())
+
