7bdd48e3501b24e4c0d6f9157ca7110bc54a497c
Kyle <c0zychair@outlook.com>
2024-12-24 09:00:32 -0600

    done

--- 07_decorators/07_03_text_decoration.py
+++ 07_decorators/07_03_text_decoration.py
@@ -7,3 +7,18 @@
 # ******************************
 # Hello
 # ******************************
+
+def decorate(symbol):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            decoration = symbol * (len(result) + 4)
+            return f"{decoration}\n{symbol} {result} {symbol}\n{decoration}"
+        return wrapper
+    return decorator
+
+@decorate("*")
+def get_user_message():
+    return input("Enter your message: ")
+
+print(get_user_message())
