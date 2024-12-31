1a7e43f22629e9fbbebc9025a9284befba3b46b1
Kyle <c0zychair@outlook.com>
2024-12-31 16:08:32 -0600

    changes made

--- projects/make-recipe-page/app.py
+++ projects/make-recipe-page/app.py
@@ -14,6 +14,7 @@ client_id = os.getenv('REDDIT_CLIENT_ID')
 client_secret = os.getenv('REDDIT_CLIENT_SECRET')
 user_agent = os.getenv('REDDIT_USER_AGENT')
 
+
 # Create a Reddit instance
 reddit = praw.Reddit(
     client_id=client_id,
